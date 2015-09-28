cecb0469772e027249f4351ae3d3ab355441f40d
ezebeltran <marceloe.beltran@gmail.com>
2015-09-27 21:49:23 -0300

    modificacion

--- cursos/menu.js
+++ cursos/menu.js
@@ -10,9 +10,19 @@ event.preventDefault();
 $("#subitem2").slideToggle(800);
 });
 
+$("#titulo3").click(function(event){
+event.preventDefault();
+$("#subitem3").slideToggle(800);
+});
+
 $("#titulo4").click(function(event){
 event.preventDefault();
 $("#subitem4").slideToggle(800);
+});
+
+$("#titulo5").click(function(event){
+event.preventDefault();
+$("#subitem5").slideToggle(800);
 });
 
 
